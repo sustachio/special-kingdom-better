8e189c3647af22cade68f4d3d2e9b1de44ccee82
sustachio <sustachio@gmail.com>
2022-08-05 18:04:27 -0500

    hgvfd

---
+++ Assets/Scripts/FlipGravityOnCollision.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipGravityOnCollision : MonoBehaviour
+{
+    bool normalGravity = true;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("b");
+        if (collision.gameObject.layer == 6)
+        {
+            normalGravity = !normalGravity;
+            Debug.Log("a");
+            Physics.gravity = new Vector3(0, normalGravity?-9.8f:-9.8f, 0);
+        }
+    }
+}
