026233abbb3c2c121de7eef0242adb548221710b
sustachio <sustachio@gmail.com>
2022-08-06 14:43:00 -0500

    lightning

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class Death : MonoBehaviour
 {
     [SerializeField] GameObject car;
+    [SerializeField] GameObject lightning;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Road"))
         {
             Instantiate(car, new Vector3(-25, -3, 0), Quaternion.identity);
+        } 
+        else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Cloud"))
+        {
+            Instantiate(lightning, new Vector3(-25, 38, 0), Quaternion.identity);
         }
-        else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Car"))
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Car"))
         {
             Time.timeScale = 0;
         }
