588345b2690293dbce8b581ce6210b476c5115ea
sustachio <sustachio@gmail.com>
2022-08-06 09:57:06 -0500

    reset for death

---
+++ Assets/Scripts/ResetLevelWtihButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetLevelWtihButton : MonoBehaviour
+{
+    [SerializeField]
+    KeyCode keyRestart;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(keyRestart))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Time.timeScale = 1;
+        }
+    }
+}
